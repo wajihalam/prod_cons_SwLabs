86e40637bc957b8a71904c6809d3645af5d669b2
Mohammad Wajih Alam <wajih.alam@usask.ca>
2021-09-09 08:56:53 -0400

    Implemented prod_cons based robot navigation
    1) Base working version of robot_navigation system based on prod_cons
    
    Next steps:
    2) Add comments for clarity. Check variable names and other stuff to improve clarity.
    
    3) Add 2 command line operations:
    
    3A)  To take user input for consumer wake up time.
    
    3B)  Take user input for buffer size. Make buffer size dynamic. Can use Malloc for it.

--- main.c
+++ main.c
@@ -3,62 +3,128 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <time.h>
 #include <semaphore.h>
+#include <stdint.h>
 
-#define THREAD_NUM 8
+#define THREAD_NUM 2
 
 sem_t semEmpty;
 sem_t semFull;
 
 pthread_mutex_t mutexBuffer;
 
-int buffer[10];
-int count = 0;
+#define BUFFERSIZE 10
+char buffer[BUFFERSIZE];
+
+const char initial_seq [5] = "+++M";
+const char exit_seq [5] = "EXIT";
+const char move_dir_north [1] = "N";
+const char move_dir_south [1] = "S";
+const char move_dir_east [1] = "E";
+const char move_dir_west [1] = "W";
+
+int charArrayToInt(char *arr) {
+    int i, value, r, flag;
+
+    flag = 1;
+    i = value = 0;
+
+    for( i = 5 ; i<7 ; ++i){
+        // if arr contain negative number
+        if( i==0 && arr[i]=='-' ){
+            flag = -1;
+            continue;
+        }
+        r = arr[i] - '0';
+        value = value * 10 + r;
+    }
+
+    value = value * flag;
+
+    return value;
+
+}
+
+void get_user_input(char *input) {
+    int length;
+    *input = '\0';
+    do {
+        if (fgets(buffer, BUFFERSIZE, stdin) == NULL) {
+            break;
+        }
+        length = strlen(buffer)-1;
+        buffer[length] = '\0';
+    } while (length == 0);
+}
+
+const char* buffer_test = buffer;
 
 void* producer(void* args) {
     while (1) {
-        // Produce
-        int x = rand() % 100;
-        sleep(1);
-
         // Add to the buffer
         sem_wait(&semEmpty);
         pthread_mutex_lock(&mutexBuffer);
-        buffer[count] = x;
-        count++;
+        printf("Enter your command\n");
+        get_user_input(buffer);
+        const char* buffer_test = buffer;
         pthread_mutex_unlock(&mutexBuffer);
         sem_post(&semFull);
+        sleep(1);
     }
 }
 
 void* consumer(void* args) {
     while (1) {
-        int y;
-
         // Remove from the buffer
         sem_wait(&semFull);
         pthread_mutex_lock(&mutexBuffer);
-        y = buffer[count - 1];
-        count--;
+        int steps = charArrayToInt(buffer);
+        if ((strncmp(buffer, initial_seq, 4) == 0)
+            && ((strncmp((buffer_test+4), move_dir_north, 1) == 0) || (strncmp((buffer_test+4), move_dir_south, 1) == 0) || (strncmp((buffer_test+4), move_dir_east, 1) == 0) || (strncmp((buffer_test+4), move_dir_west, 1) == 0) ))
+        {
+            if(strncmp((buffer_test+4), move_dir_north, 1) == 0)
+            {
+
+                printf("Moved North %d steps\n", steps);
+            }
+            else if(strncmp((buffer_test+4), move_dir_south, 1) == 0)
+            {
+                printf("Moved South %d steps\n", steps);
+            }
+            else if(strncmp((buffer_test+4), move_dir_east, 1) == 0)
+            {
+                printf("Moved East %d steps\n", steps);
+            }
+            else if(strncmp((buffer_test+4), move_dir_west, 1) == 0)
+            {
+                printf("Moved West %d steps\n", steps);
+            }
+        }
+        else if(strncmp((buffer), exit_seq, 4) == 0)
+        {
+            printf("Exited\n");
+            pthread_kill(&producer, SIGINT);
+            pthread_kill(&consumer, SIGINT);
+
+            exit(0);
+        }
+        else
+        {
+            printf("Invalid Command\n");
+        }
         pthread_mutex_unlock(&mutexBuffer);
         sem_post(&semEmpty);
-
-        // Consume
-        printf("Got %d\n", y);
-        sleep(1);
     }
 }
 
 int main(int argc, char* argv[]) {
-    srand(time(NULL));
     pthread_t th[THREAD_NUM];
     pthread_mutex_init(&mutexBuffer, NULL);
     sem_init(&semEmpty, 0, 10);
     sem_init(&semFull, 0, 0);
     int i;
     for (i = 0; i < THREAD_NUM; i++) {
-        if (i > 0) {
+        if (i %2 == 0) {
             if (pthread_create(&th[i], NULL, &producer, NULL) != 0) {
                 perror("Failed to create thread");
             }
